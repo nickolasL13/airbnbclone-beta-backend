c9207cecd74141df3dd9f8a1ce3100aad4cf89c1
Your Name <nickk.lima17@gmail.com>
2021-11-15 22:45:03 -0300

    Att cors

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,17 @@ import morgan from 'morgan';
 import errorHandler from 'errorhandler';
 import { json } from 'body-parser';
 import router from './webServer/routes/mongoRoutes';
+import cors from 'cors';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use(json())
+app.use(json());
+app.use(cors({origin: `https://ws-airbnbclone-1226.herokuapp.com}`}));
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'));
-    app.use(errorHandler())
+    app.use(errorHandler());
 } else {
     app.use(morgan('tiny'));
 }
